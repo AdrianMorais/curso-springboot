f2916081b8e2dfb8b548d5cd001c4016ded2cc97
AdrianMorais <adrian.morais4448@gmail.com>
2020-05-18 15:30:07 -0300

    Camada serviço, registro de componentes

--- src/main/java/com/curso/resources/UserResource.java
+++ src/main/java/com/curso/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.entities.Usuario;
+import com.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> findAll() {
-		Usuario u = new Usuario(1L,"Maria","Maria@gmada.com","9999999","admin");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>> findAll() {
+		List<Usuario> list = service.findAll(); 
+		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
---
+++ src/main/java/com/curso/services/UserService.java
@@ -0,0 +1,27 @@
+package com.curso.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.entities.Usuario;
+import com.curso.repositories.UserRepository;
+import com.sun.el.stream.Optional;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<Usuario> findAll() {
+		return repository.findAll();
+	}
+	
+	public Usuario findById(Long id) {
+		java.util.Optional<Usuario> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
